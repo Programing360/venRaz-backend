b4b3994b3f2ef0d7febf1d0a233701a2ba7848a6
rezwanasharmin <rezwanasharmin85@gmail.com>
2026-08-29 00:03:37 +0600

    fix: resolve catchAsync merge conflict

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,23 +1,7 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-<<<<<<< HEAD
 export const catchAsync = (fn: RequestHandler): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction): void => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
   };
 };
-=======
-// Asynchronous controller-এর টাইপ সংজ্ঞায়িত করা
-type AsyncRequestHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => Promise<any>;
-
-// Higher-order function যা try-catch-এর কাজ স্বয়ংক্রিয়ভাবে করে
-export const catchAsync = (fn: AsyncRequestHandler): RequestHandler => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch((err) => next(err));
-  };
-};
->>>>>>> origin/main

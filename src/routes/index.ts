090bcb238e5102bf93d9a0b02ac11331657abe22
rezwanasharmin <rezwanasharmin85@gmail.com>
2026-08-29 16:20:25 +0600

    feat(wishlist): implement wishlist CRUD services and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
-import { Router } from "express";
-import { UserRoutes } from "../models/user/user.route";
+import { Router } from 'express';
+import { UserRoutes } from '../models/user/user.route';
+import { WishlistRoutes } from '../models/wishlist/wishlist.route';
 
 const router = Router();
 
 const moduleRoutes = [
   {
-    path: "/users",
+    path: '/users',
     route: UserRoutes,
+  },
+  {
+    path: '/wishlist',
+    route: WishlistRoutes,
   },
 ];
 
